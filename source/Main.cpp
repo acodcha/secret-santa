0316662053a5cdddc4a6bbffd1782c158fe2bc53
Alexandre Coderre-Chabot <alexandre.coderre.chabot@gmail.com>
2023-09-24 15:15:21 -0700

    Add Main.cpp

---
+++ source/Main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  std::cout << "Start of program." << std::endl;
+  std::cout << "End of program." << std::endl;
+  return EXIT_SUCCESS;
+}
